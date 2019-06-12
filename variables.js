4ff6a8e183b004fcd55cb1e831831fe43eb7396d
steph5000 <51511634+steph5000@users.noreply.github.com>
2019-06-12 19:11:56 +0200

    variables

--- variables.js
+++ variables.js
@@ -1,1 +1,8 @@
 // you solution should be here
+var birthyear = 1994, futureyear = 2020;
+var posibleage1 =  26, possibleage2 = 25;
+var currentage = 24;
+var maxage = 100;
+var amount = 3;
+var total = maxage * amount;
+var screen = "you will need" + "" + total + "" + "to last you until the ripe old age of" + "" + maxage;
